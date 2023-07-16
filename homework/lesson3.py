cdf82f01953a696afac7c13d7a122e34ce8d5c9d
soulfly2007 <andreymekhedko@gmail.com>
2023-07-16 20:59:32 +0300

    lesson3 changed

--- homework/lesson3.py
+++ homework/lesson3.py
@@ -3,18 +3,14 @@
 # •	Найти самое длинное слово в введенном предложении. Учтите что в предложении есть знаки препинания.
 from string import punctuation
 s = input('Enter a string: ')
-s = ''.join(a for a in s if a not in punctuation)
-print(max(s.split(), key=len))
+s = [a.strip(",.:;!?") for a in s.split()]
+print(max(s, key=len))
 
 # •	Вводится строка. Требуется удалить из нее повторяющиеся символы и все пробелы. Например, если было введено "abc cde def", то должно быть выведено "abcdef".
 # s ='abc cde def'
 s = input('Enter a string: ')
-s = ''.join(s.split())
-s1 = ''
-for i in s:
-    if i not in s1:
-        s1 += i
-print(s1)
+s = ''.join(sorted(set(s.replace(' ', ''))))
+print(s)
 
 # •	Посчитать количество строчных (маленьких) и прописных (больших) букв в введенной строке. Учитывать только английские буквы.
 # s ='abc CDE def АБВ'
